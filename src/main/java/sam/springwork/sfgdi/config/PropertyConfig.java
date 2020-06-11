e772747ff6827fdd4c09dde566d6b214731208d0
sam.thangiah <sam.thangiah@AT250XG6MLFH2F.srunet.sruad.edu>
2020-06-11 00:31:30 -0400

    Spring Environment Variables

--- src/main/java/sam/springwork/sfgdi/config/PropertyConfig.java
+++ src/main/java/sam/springwork/sfgdi/config/PropertyConfig.java
@@ -1,10 +1,12 @@
 package sam.springwork.sfgdi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import sam.springwork.sfgdi.examplebeans.FakeDataSource;
 
@@ -12,6 +14,9 @@ import sam.springwork.sfgdi.examplebeans.FakeDataSource;
 @PropertySource("classpath:datasource.properties")  //look inside the properties files to resolve the string values
 
 public class PropertyConfig {
+	
+	@Autowired
+	Environment env;
 	
 	@Value("${sam.username}") //sam.username is how it is declared in the datasource.properties
 	String user;
@@ -25,7 +30,8 @@ public class PropertyConfig {
 	@Bean //Get instance of FakeDataSource class and load it with data from the properties file
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(user);
+		fakeDataSource.setUser(env.getProperty("SAM_USERNAME"));  //environment variables
+		//fakeDataSource.setUser(user);
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		
